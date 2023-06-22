955c4cffc410c257cc3dad8251e68b64a5adc2f9
leonidaspedro <leonidas@unochapeco.edu.br>
2023-06-22 15:00:24 -0300

    feat/product

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :products
   get 'welcome/index'
   root 'welcome#index'
   resources :coins
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_06_13_181304) do
+ActiveRecord::Schema.define(version: 2023_06_14_203744) do
 
   create_table "coins", force: :cascade do |t|
     t.string "description"
     t.string "acronym"
     t.string "url_image"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "products", force: :cascade do |t|
+    t.string "description"
+    t.float "price"
+    t.integer "amount"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
